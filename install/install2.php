899c335dc71c6b2f785a9831271935cfd8591007
Cristina Ibañez Lendines <taiga098@hotmail.com>
2025-03-31 14:59:52 +0200

    update headers

--- install/install2.php
+++ install/install2.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 This file is part of McWebPanel.
 Copyright (C) 2020-2025 DEV-MCWEBPANEL
@@ -19,12 +18,13 @@ Copyright (C) 2020-2025 DEV-MCWEBPANEL
 */
 
 header("Content-Security-Policy: default-src 'none'; style-src 'self'; img-src 'self'; script-src 'self'; form-action 'self'; base-uri 'none'; connect-src 'self'; frame-ancestors 'none'");
+header("Cross-Origin-Resource-Policy: same-origin");
 header('X-Content-Type-Options: nosniff');
 header('Strict-Transport-Security: max-age=63072000; includeSubDomains; preload');
 header("X-XSS-Protection: 1; mode=block");
 header("Referrer-Policy: no-referrer");
 header('Permissions-Policy: geolocation=(), microphone=()');
-header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Cache-Control: private, no-cache, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0');
 
