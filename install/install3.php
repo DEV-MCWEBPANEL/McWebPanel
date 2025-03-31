2124bdb46f1faf3a3debbf872a0db8b1ffffa095
Cristina Ibañez Lendines <taiga098@hotmail.com>
2025-03-31 15:19:45 +0200

    fix dash duplicated

--- install/install3.php
+++ install/install3.php
@@ -338,7 +338,7 @@ require_once "../template/errorreport.php";
         fwrite($file, 'define("CONFIGBACKUROTATE", "0");' . PHP_EOL);
         fwrite($file, 'define("CONFIGZONAHORARIA", "' . $elzonahoraria . '");' . PHP_EOL);
         fwrite($file, 'define("CONFIGOPTIONRECREATEREGIONFILES", "0");' . PHP_EOL);
-        fwrite($file, 'define("CONFIGOPTIONRENDERDEBUGLABELS", , "0");' . PHP_EOL);
+        fwrite($file, 'define("CONFIGOPTIONRENDERDEBUGLABELS", "0");' . PHP_EOL);
         fwrite($file, "?>" . PHP_EOL);
         fclose($file);
 
