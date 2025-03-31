1e39a9fec7a276a3ed0a83b384e6fb38bf5a88de
Cristina Ibañez Lendines <taiga098@hotmail.com>
2025-03-31 15:47:06 +0200

    missing isset con function test_input

--- tareas.php
+++ tareas.php
@@ -81,10 +81,12 @@ if (isset($_SESSION['CONFIGUSER']['psystemconftemaweb'])) {
 
         function test_input($data)
         {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
+            if (isset($data)) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
         }
     ?>
 
