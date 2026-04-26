a966f1b440664b279bdb42919b7a547d36ac90db
DEV-MCWEBPANEL <taiga098@hotmail.com>
2026-04-26 05:22:16 +0200

    delete mod_php7
    - Se elimina mod php7, ya que la version minima soportada del panel es php 8.0

--- install/install3.php
+++ install/install3.php
@@ -213,13 +213,6 @@ require_once "../template/errorreport.php";
         $linea3 = "php_value max_file_uploads 1";
 
         $file = fopen($rutaescribir, "w");
-        fwrite($file, "<IfModule mod_php7.c>" . PHP_EOL);
-        fwrite($file, $linea1 . PHP_EOL);
-        fwrite($file, $linea2 . PHP_EOL);
-        fwrite($file, $linea3 . PHP_EOL);
-        fwrite($file, "php_value max_execution_time 600" . PHP_EOL);
-        fwrite($file, "php_value max_input_time 600" . PHP_EOL);
-        fwrite($file, "</IfModule>" . PHP_EOL);
         fwrite($file, "<IfModule mod_php8.c>" . PHP_EOL);
         fwrite($file, $linea1 . PHP_EOL);
         fwrite($file, $linea2 . PHP_EOL);
